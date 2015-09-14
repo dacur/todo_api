d334dda13222aed13f6f1c7cb4b70fae20bf76d5
dacur <dacur7@gmail.com>
2015-09-14 10:18:06 -0400

    Fixes Todo controller and index test.

--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -1,13 +1,15 @@
 require "spec_helper"
 
 RSpec.describe TodosController, type: :controller do 
+	# subject(:todo) { create(:todo) }
 	describe "GET /index" do
-		let(:todo) { FactoryGirl.create :todo }
+		let(:todo) { create(:todo) }
 		it "returns a todo list" do
+			todo
 			get :index, format: :json
 			expect(response).to have_http_status(:ok)
-			todo_json = response_json["todos"]
-			expect(todo_json["title"]).to eq("Buy groceries")
+			todo_json = JSON.parse(response.body)
+			expect(todo_json.first["title"]).to eq("Go shopping")
 		end
 	end
 
