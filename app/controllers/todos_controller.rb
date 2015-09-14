98ee0476e8acb3c5095b6d723ebf6ee2c2525404
dacur <dacur7@gmail.com>
2015-09-14 11:08:53 -0400

    Fixes GET, adds POST methods and tests for Todo controller.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,12 +5,12 @@ class TodosController < ApplicationController
 
 	def create
 		@todo = Todo.create(todo_params)
-		render json: @todo
+		render json: @todo, status: 201
 	end
 
 	private 
 
 	def todo_params
-		params.require(todo).permit(:title, :is_completed)
+		params.require(:todo).permit(:title, :is_completed)
 	end 
 end
--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -13,5 +13,17 @@ RSpec.describe TodosController, type: :controller do
 		end
 	end
 
+	describe "POST /create" do
+		context "with valid params" do
+			let(:valid_params) { attributes_for(:todo, title: "Go shopping") }
+			it "creates a todo list" do
+				post :create, todo: valid_params, format: :json
+				expect(response).to have_http_status(:created)
+				todo_json = JSON.parse(response.body)
+				expect(todo_json["title"]).to eq("Go shopping")
+			end
+		end
+	end
+
 	
 end
