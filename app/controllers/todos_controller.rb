0e10307fc30879f540abe4d38ff1cb5b1452f688
dacur <dacur7@gmail.com>
2015-09-14 12:45:12 -0400

    Fixes indentation in controller.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,38 +1,38 @@
 class TodosController < ApplicationController
-	def index
-		render json: Todo.all 
-	end
+  def index
+    render json: Todo.all 
+  end
 
-	def create
-		@todo = Todo.new(todo_params)
-		if @todo.save
-			render json: @todo, status: 201
-		else
-			render json: @todo, status: 422
-		end
-	end
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render json: @todo, status: 201
+    else
+      render json: @todo, status: 422
+    end
+  end
 
-	def update
-		@todo = Todo.find(params[:id])
-		if @todo.update_attributes(todo_params)
-			render json: @todo, status: :ok
-		else
-			render json: @todo, status: 422
-		end
-	end
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update_attributes(todo_params)
+      render json: @todo, status: :ok
+    else
+      render json: @todo, status: 422
+    end
+  end
 
-	def destroy
-		@todo = Todo.find(params[:id])
-		if @todo.destroy
-			render json: nil, status: :ok
-		else
-			render json: @todo, status: :error
-		end
-	end
+  def destroy
+    @todo = Todo.find(params[:id])
+    if @todo.destroy
+      render json: nil, status: :ok
+    else
+      render json: @todo, status: :error
+    end
+  end
 
-	private 
+  private 
 
-	def todo_params
-		params.require(:todo).permit(:title, :is_completed)
-	end 
+  def todo_params
+    params.require(:todo).permit(:title, :is_completed)
+  end 
 end
